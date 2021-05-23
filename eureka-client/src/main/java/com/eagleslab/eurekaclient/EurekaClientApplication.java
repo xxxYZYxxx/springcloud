f9f2ada1edc107dd1ce384cf2b79ca836117e475
xxxYZYxxx <1542049001@qq.com>
2021-05-23 10:23:34 +0800

    523 add

---
+++ eureka-client/src/main/java/com/eagleslab/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,23 @@
+package com.eagleslab.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @RequestMapping("/sayHello")
+    public String sayHello(String name){
+        return name+",你好！";
+    }
+
+}
